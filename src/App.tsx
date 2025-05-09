a6c32197923dc48db30a4238e2dc9cd256887c84
Oleh Tymoshchuk <pryge321@gmail.com>
2025-05-09 10:23:39 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import { Navbar } from './components/Navbar';
-import { PeoplePage } from './components/Peoplepage';
+import { PeoplePage } from './components/PeoplePage';
 //bla bla bla
 
 export const App = () => (
